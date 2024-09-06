b8e24480ece39e42783354b457cc144c7965df48
aalleexxaannddrr-prog <aalleexxaannddrr2003@list.ru>
2024-09-06 15:55:20 +0300

    Add files via upload

---
+++ src/main/java/com/example/demo/ItmoRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItmoRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItmoRestaurantApplication.class, args);
+	}
+
+}
